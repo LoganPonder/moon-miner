04a65229cd551449457a2d0e88b844fb08a7465a
LoganPonder <carl.ponder7@gmail.com>
2021-03-28 08:39:40 -0600

    Completed functionality/UI

--- app.js
+++ app.js
@@ -4,17 +4,6 @@ let total = document.getElementById("total-points");
 let multiplier = document.getElementById("multiplier");
 let autoPerSecond = 0;
 
-// add second 'type'argument to the buyItem function for auto/click
-// let upgrades ={
-//     auto: {
-
-//     },
-//     click: {
-
-//     }
-// }
-
-
 // CLICK UPGRADES
 let clickUpgrades = {
     item1: {
@@ -38,7 +27,8 @@ let autoUpgrades = {
     item4: {
         price: 4,
         quantity: 0,
-        multiplier: 5 
+        multiplier: 5,
+        auto: 5, 
     }
 }
 
@@ -46,7 +36,6 @@ let autoUpgrades = {
 function addPoint() {
     let perClick = 1 * totalMultiplier;
     totalPoints += perClick;
-    console.log(`Total Points: ${totalPoints}`);
     update()
 }
 
@@ -66,12 +55,9 @@ function collectAutoUpgrades(){
     }     
     document.getElementById('ppp').innerText = autoPerSecond;
     totalPoints += autoPerSecond;
-    // console.log(totalPoints);
-    console.log(`Points Per Second: ${autoPerSecond}`);
     update();
 }
 
-// can add a second 'type'argument to the buy item function referencing the upgrades object commented out at the top of the page
 function buyItem(currentitem) {
     let item;
     if(currentitem === 'item4') {
